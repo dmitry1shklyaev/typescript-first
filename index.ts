67af6058e577752a79a71e412f9e33cd7dd10b45
Dmitry Shklyaev <shklyaev1dmitry@gmail.com>
2026-08-21 11:34:52 +0400

    ...

---
+++ index.ts
@@ -0,0 +1,23 @@
+function findTaskById(tasks: Task[], id: number) : Task | undefined{
+  return tasks.find((task) => task.id === id);
+}
+
+type Task = {
+  id: number,
+  title: string,
+  completed: boolean,
+  priority: number,
+  description?: string
+}
+
+const tasks: Task[] = [
+  {id: 1, title: "Задача 1", completed: true, priority: 1, description: "Описание задачи 1"},
+  {id: 2, title: "Задача 2", completed: true, priority: 2},
+  {id: 3, title: "Задача 3", completed: true, priority: 3, description: "Описание задачи 3"}
+];
+
+const foundTask = findTaskById(tasks, 2);
+const missingTask = findTaskById(tasks, 10);
+
+foundTask ? console.log(`${foundTask.title}`) : console.log(`Задача не найдена`);
+missingTask ? console.log(`${missingTask.title}`) : console.log(`Задача не найдена`);
